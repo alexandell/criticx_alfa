654350d2db616572d509d8abf5cb90991bf0ef13
alexandell <alexander.chavarria@utec.edu.pe>
2020-05-28 00:20:35 -0500

    load data

--- app/models/involved_company.rb
+++ app/models/involved_company.rb
@@ -1,6 +1,5 @@
 class InvolvedCompany < ApplicationRecord
   belongs_to :game
   belongs_to :company
-  validates :developer, presence: true
-  validates :publisher, presence: true
+  validates :developer, :publisher, inclusion: { in: [ true, false ] }
 end
