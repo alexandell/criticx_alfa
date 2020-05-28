0df3063023f4b790662e002969a22890b0447e8a
alexandell <alexander.chavarria@utec.edu.pe>
2020-05-27 20:52:49 -0500

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
     has_many :reviews
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validate :validate_age
+  private
+  def validate_age
+    if birth_date.present? && birth_date > 16.years.ago.to_date
+      errors.add(:birth_date, "You should be 16 years old to create an account")
+    end
+  end
 end
