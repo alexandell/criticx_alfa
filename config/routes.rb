93a6f8b559f36bf189540014b1b33cd4b92e66f1
alexandell <alexander.chavarria@utec.edu.pe>
2020-06-01 11:05:57 -0500

    Create the index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+  resources :games
+  resources :companies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
