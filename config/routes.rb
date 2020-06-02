e3db077270c6ef0d11bb3dad1fc2b71973383e9f
alexandell <alexander.chavarria@utec.edu.pe>
2020-06-01 20:33:44 -0500

    Create a form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'companies/index'
-  resources :games
-  resources :companies
+  resources :games, only:[:index, :show, :new, :create, :edit, :update]
+  resources :companies, only:[:index, :show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
